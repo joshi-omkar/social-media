5a1fbbc278e240431ebff3a0e550a847edb604a5
joshi-omkar <joshiomkar0400@gmail.com>
2023-06-24 09:01:55 +0530

    funtionality: other required functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { makeServer } from "./server";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import {store} from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 makeServer();
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
