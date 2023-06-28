f37c07cd1eb6e289fdd1d4d1a90db601d6403b86
joshi-omkar <joshiomkar0400@gmail.com>
2023-06-28 14:26:13 +0530

    Functionality: created add post

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './features/auth/authSlice'
+import authReducer from "./features/auth/authSlice";
+import newPostReducer from "./features/newPostSlice/newPostSlice";
+import { feedReducer } from "./features/Feed/FeedSlice";
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
-  }
-})
+    auth: authReducer,
+    newPost: newPostReducer,
+    feed: feedReducer,
+  },
+});
