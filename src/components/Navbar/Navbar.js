9784464d6bf9b56210a0a755d04bc5eee952e66c
joshi-omkar <joshiomkar0400@gmail.com>
2023-07-02 12:31:51 +0530

    Style: Added logos in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,16 +3,20 @@ import "./Navbar.css";
 import SearchBar from "./SearchBar";
 import ProfileDropdown from "./ProfileDropdown";
 import { Link } from "react-router-dom";
+import Fomo from "../../assets/Fomo";
+import ExploreSVG from "../../assets/ExploreSVG";
+import BookmarkNav from "../../assets/BookmarkNav";
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <Link to='/' className="logo">FOMO</Link>
+      <Link to='/' className="logo" style={{display: "flex"}}><Fomo/></Link>
       <div style={{ width: "30%" }}>
         <SearchBar />
       </div>
-      <div>
-        <Link to={'/explore'}>Explore</Link>
+      <div style={{display: 'flex',width: '200px', gap: '20px'}}>
+        <Link to={'/explore'}><ExploreSVG/></Link>
+        <Link to={'/bookmark'}><BookmarkNav/></Link>
       </div>
       <ProfileDropdown />
     </div>
