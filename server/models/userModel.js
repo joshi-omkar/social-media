7c3fb0231ddfcf5e6ea558d97a245ce4c7aa2215
joshi-omkar <joshiomkar0400@gmail.com>
2023-09-06 13:21:26 +0530

    Adding backend

---
+++ server/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    bio: { type: String },
+    profileAvatar: { type: String, required: true },
+    password: { type: String, required: true },
+    followers: [
+      {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, required: true },
+        username: { type: String, required: true },
+        profileAvatar: { type: String, required: true },
+      },
+    ],
+    following: [
+      {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, required: true },
+        username: { type: String, required: true },
+        profileAvatar: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
